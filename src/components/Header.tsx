02b9c1561b9420250b6939b3c889720efe33bbee
soy eddy <soyeddy.email.com>
2025-01-05 12:05:11 -0600

    incluyendo dropdown en resoluciones moviles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -6,56 +7,70 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { X } from "lucide-react";
 import SearchWeather from "./SearchWeather";
+import DropDown from "./DropDown";
 
 interface HeaderProps {
-    onAddCity: (newCity: string) => void;
-    }
-    
-const Header: React.FC<HeaderProps> = ({onAddCity}) => (
-    
+  onAddCity: (newCity: string) => void;
+}
 
-  <header className=" p-4 shadow-md">
-    <nav className="max-w-7xl mx-auto flex justify-between items-center">
-      <div className=" text-3xl font-semibold">TimeApp</div>
+const Header: React.FC<HeaderProps> = ({ onAddCity }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-      <NavigationMenu className="flex space-x-6 ">
-        <NavigationMenuList className="flex space-x-6">
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className=" p-2 rounded-lg">
-              Item One
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Valor 1</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+  return (
+    <header className="p-4 shadow-md">
+      <nav className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="text-3xl font-semibold">TimeApp</div>
 
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className=" p-2 rounded-lg">
-              Item Two
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Valor 2</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+          <div
+            className="lg:hidden p-2"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          >
+            {isMobileMenuOpen ? <X className="h-6 w-6" /> : <DropDown />}
+          </div>
 
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="p-2 rounded-lg">
-              Item Three
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Valor 3</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+          <div className="hidden lg:flex items-center space-x-6">
+            <NavigationMenu>
+              <NavigationMenuList className="flex space-x-6">
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="p-2 rounded-lg">
+                    Item One
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink>Valor 1</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
 
-      <div className="flex justify-center items-center">
-        <SearchWeather onAddCity={onAddCity} />
-      </div>
-    </nav>
-  </header>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="p-2 rounded-lg">
+                    Item Two
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink>Valor 2</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
 
-);
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="p-2 rounded-lg">
+                    Item Three
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink>Valor 3</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+
+            <div className="flex justify-center items-center">
+              <SearchWeather onAddCity={onAddCity} />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,19 +1,20 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import { FC, PropsWithChildren } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 interface MainLayoutProps extends PropsWithChildren {
-    onAddCity: (newCity: string) => void;
-  }
+  onAddCity: (newCity: string) => void;
+}
 
-
-const MainLayout: FC<MainLayoutProps> = ({ children , onAddCity }) => {
+const MainLayout: FC<MainLayoutProps> = ({ children, onAddCity }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header onAddCity={onAddCity} />
 
-      <main className="flex-grow flex justify-center items-center">
-        {children}
+      <main className="flex-grow flex justify-center items-center w-full px- sm:px-6 lg:px-8">
+        <div className="w-full max-w-7xl mx-auto">
+          {children}
+        </div>
       </main>
 
       <Footer />
