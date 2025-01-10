dfdfd8b67b224d9cf2497fe5f14718c452944ea1
soy eddy <soyeddy.email.com>
2025-01-09 21:55:47 -0600

    separando logica usando customHooks

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,80 +1,20 @@
-import { ChangeEvent, useState, useEffect } from "react";
+import { useSearchWeather } from "@/hooks/useSearchWeather";
 import { Input } from "./ui/input";
-import axios from "axios";
 import { City } from "@/types";
-import { API_KEY, API_URL_GEO } from "@/constants/api";
 
 interface SearchWeatherProps {
   onAddCity: (newCity: City) => void;
 }
 
 const SearchWeather = ({ onAddCity }: SearchWeatherProps) => {
-  const [inputValue, setInputValue] = useState("");
-  const [cities, setCities] = useState<City[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  const hour = new Date().getHours();
-  const isDaytime = hour >= 6 && hour < 18;
-
-  // Obtener las clases de color basadas en el momento del día
-  const getColorClasses = () => {
-    if (isDaytime) {
-      return {
-        background: "bg-white/10",
-        text: "text-gray-900",
-        accent: "text-blue-600",
-        border: "border-white/20",
-        shadow: "shadow-light",
-      };
-    } else {
-      return {
-        background: "bg-black/20",
-        text: "text-white",
-        accent: "text-blue-300",
-        border: "border-white/10",
-        shadow: "shadow-dark",
-      };
-    }
-  };
-
-  const colors = getColorClasses();
-
-  useEffect(() => {
-    const fetchCities = async () => {
-      if (inputValue.length < 3) {
-        setCities([]);
-        return;
-      }
-
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          `${API_URL_GEO}q=${inputValue}&limit=5&appid=${API_KEY}`
-        );
-        const data = response.data;
-        setCities(data);
-      } catch (error) {
-        console.error("Error fetching cities:", error);
-        setCities([]);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const debounceTimer = setTimeout(fetchCities, 300);
-    return () => clearTimeout(debounceTimer);
-  }, [inputValue]);
-
-  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleCitySelect = (selectedCity: City) => {
-    const cleanCityName = selectedCity.name.replace(/\(.*\)/, "").trim(); // Eliminar el texto entre paréntesis y los espacios adicionales
-    onAddCity({ name: cleanCityName, country: selectedCity.country });
-    setInputValue("");
-    setCities([]);
-  };
+  const {
+    inputValue,
+    onInputChange,
+    cities,
+    loading,
+    handleCitySelect,
+    colors,
+  } = useSearchWeather({ onAddCity });
 
   return (
     <div className="relative">
