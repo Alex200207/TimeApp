071a510e8ad5f864c619f6cc392e7e3b9e8e609c
soy eddy <soyeddy.email.com>
2025-01-10 19:53:11 -0600

    corrijiendo error de interfaz en resoluciones moviles.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,25 +12,24 @@ const Header: React.FC<HeaderProps> = ({ onAddCity }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const hour = new Date().getHours();
   const isDaytime = hour >= 6 && hour < 18;
-  
 
   // Obtener las clases de color basadas en el momento del día
   const getColorClasses = () => {
     if (isDaytime) {
       return {
-        background: 'bg-white/10',
-        text: 'text-gray-900',
-        accent: 'text-blue-600',
-        border: 'border-white/20',
-        shadow: 'shadow-light'
+        background: "bg-white/10",
+        text: "text-gray-900",
+        accent: "text-blue-600",
+        border: "border-white/20",
+        shadow: "shadow-light",
       };
     } else {
       return {
-        background: 'bg-black/20',
-        text: 'text-white',
-        accent: 'text-blue-300',
-        border: 'border-white/10',
-        shadow: 'shadow-dark'
+        background: "bg-black/20",
+        text: "text-white",
+        accent: "text-blue-300",
+        border: "border-white/10",
+        shadow: "shadow-dark",
       };
     }
   };
@@ -38,10 +37,14 @@ const Header: React.FC<HeaderProps> = ({ onAddCity }) => {
   const colors = getColorClasses();
 
   return (
-    <header className={`p-4 ${colors.shadow} z-50 ${colors.background} backdrop-blur- border-b ${colors.border}`}>
+    <header
+      className={`p-4 ${colors.shadow} z-50 ${colors.background} backdrop-blur- border-b ${colors.border}`}
+    >
       <nav className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center space-x-4">
-          <div className={`text-2xl font-semibold flex-1 text-center lg:text-left ${colors.text}`}>
+          <div
+            className={`text-2xl font-semibold flex-1 text-center lg:text-left ${colors.text}`}
+          >
             <span className={colors.accent}>Time</span>App
           </div>
 
@@ -56,23 +59,14 @@ const Header: React.FC<HeaderProps> = ({ onAddCity }) => {
             <DropDown />
           </div>
 
-          <div className={`hidden lg:flex items-center space-x-6 ${colors.text}`}>
+          <div
+            className={`hidden lg:flex items-center space-x-6 ${colors.text}`}
+          >
             <MenuNavigator />
             <MenuNavigator />
             <MenuNavigator />
           </div>
         </div>
-
-     
-        {isMobileMenuOpen && (
-          <div className={`lg:hidden mt-4 p-4 rounded-lg ${colors.background} backdrop-blur-xl ${colors.border}`}>
-            <div className="flex flex-col space-y-4">
-              <MenuNavigator />
-              <MenuNavigator />
-              <MenuNavigator />
-            </div>
-          </div>
-        )}
       </nav>
 
       <style>{`
