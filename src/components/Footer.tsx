dbcc48df7da2d67b00f77f226aacd92cbb31400d
soy eddy <soyeddy.email.com>
2025-01-13 19:31:06 -0600

    mejorando footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
-
+import { Github } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 bg-transparent text-white text-center p-4">
-      <p>&copy; 2025 developed by Alex Talavera</p>
+    <footer className=" text-white text-center p-4">
+      <div className="flex flex-col items-center">
+        <p className="m-3 text-sm">&copy; {new Date().getFullYear()} TimeApp</p>
+        <a
+          href="https://github.com/Alex200207"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2 text-white hover:text-gray-400 transition-colors"
+        >
+          <Github className="w-6 h-6" />
+          <span className="font-medium">Alex Talavera</span>
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
